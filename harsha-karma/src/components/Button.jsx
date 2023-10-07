2b52d7a8fdd994c2ed53f2e0ed5ad5c930738dca
sri13.vinukonda@gmail.com <>
2023-10-07 12:35:45 -0400

    Added Button component and style PROP setup tbd

---
+++ harsha-karma/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import '../styles/Button.css'
+
+function Button(props) {
+    // Send the title name in item props
+  return (
+    
+    <div className='buttonBody'>
+        PLACEHOLDER
+    </div>
+  );
+}
+
+export default Button;
---
+++ harsha-karma/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import '../styles/NavBar.css'
+import Button from './Button';
+
+function NavBar() {
+  return (
+    <div className='navBar'>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+    </div>
+  );
+}
+
+export default NavBar;
