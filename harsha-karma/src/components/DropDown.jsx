3de1b472b747b253f997d85d6e562bf7cd43d436
Sri Vinukonda <sri13.vinukonda@gmail.com>
2023-10-21 11:42:23 -0400

    test-commit

---
+++ harsha-karma/src/components/DropDown.jsx
@@ -0,0 +1,12 @@
+function Button(props) {
+    // Send the title name in item props
+  return (
+    
+    <div className='buttonBody'>
+        {props.buttonTitle}
+    </div>
+  );
+}
+// test commit
+
+export default Button;
