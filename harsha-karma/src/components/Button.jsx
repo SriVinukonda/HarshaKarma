ecdb8b05418e2141cc04ffb90c3e9b67ee5662bd
Sri Vinukonda <sri13.vinukonda@gmail.com>
2023-10-22 09:41:57 -0400

    Planning ahead

--- harsha-karma/src/components/Button.jsx
+++ harsha-karma/src/components/Button.jsx
@@ -2,11 +2,18 @@ import '../styles/Button.css'
 
 function Button(props) {
     // Send the title name in item props
+    
   return (
     
     <div className='buttonBody'>
         {props.buttonTitle}
+        <div>
+          {
+          // map the props.options value, and then pass each props.options value as a prop
+          }
+        </div>
     </div>
+    
   );
 }
 
