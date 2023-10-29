b7c9aa0ef9684bce65b9331c4c345c02b233f783
Sri Vinukonda <sri13.vinukonda@gmail.com>
2023-10-29 13:01:05 -0400

    More progress made on Journal Entry

--- harsha-karma/src/components/EntryLog.jsx
+++ harsha-karma/src/components/EntryLog.jsx
@@ -6,10 +6,11 @@ function EntryLog(props) {
     
     <div className='entryLogBody'>
         <p className='date'>YYYY-MM-DD</p>
-        <p>Entry Title</p>
-        {/* <p className='entryContent'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> */}
-        <p>sdfsd</p>
-    </div>
+        <div className="content">
+          <p className='entryTitle'>Entry Title</p>
+          <p className='journalEntry'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        </div>
   );
 }
 
