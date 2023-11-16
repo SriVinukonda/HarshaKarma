0224febb0330380c8f6604b04f6b5caf33a4eba1
Sri Vinukonda <sri13.vinukonda@gmail.com>
2023-11-15 23:58:28 -0500

    Changed entryLog to take in prop values

--- harsha-karma/src/components/EntryLog.jsx
+++ harsha-karma/src/components/EntryLog.jsx
@@ -7,10 +7,10 @@ function EntryLog(props) {
   return (
     
     <div className='entryLogBody'>
-        <p className='date'>YYYY-MM-DD</p>
+        <p className='date'>{props.date}</p>
         <div className="content">
-          <p className='entryTitle'>Entry Title</p>
-          <p className='journalEntry'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p className='entryTitle'>{props.title}</p>
+          <p className='journalEntry'>{props.content}</p>
         <WebGLScene/>
         </div>
         </div>
