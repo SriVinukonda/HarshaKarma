8873c401111a7e164e06ea5418840738f60a87f2
Sri Vinukonda <sri13.vinukonda@gmail.com>
2023-11-23 23:25:34 -0500

    got react-three-fiber to work

--- harsha-karma/src/components/WebGLScene.jsx
+++ harsha-karma/src/components/WebGLScene.jsx
@@ -12,13 +12,13 @@ function Box(props) {
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  useFrame((state, delta) => (ref.current.rotation.z += delta))
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
       {...props}
       ref={ref}
-      scale={clicked ? 1.5 : 1}
+      scale={clicked ? 1.5 : 3}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}>
@@ -36,9 +36,10 @@ const WebGLScene = (props) => {
   <Canvas>
     <ambientLight />
     <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
-  </Canvas>);
+    <Box position={[0, 0, 0]} />
+    
+  </Canvas>
+  );
   
 };
 
